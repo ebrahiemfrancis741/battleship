0b351adde6bc5a74a183fdcd6b441c14592babba
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-02-27 03:32:10 +0200

    Add getUiComponents() function that gets all user interface components our scripts need to control

---
+++ src/js/uiManager.js
@@ -0,0 +1,8 @@
+function getUiComponents() {
+  return {
+    playerOneboard: document.querySelector("#player1-board"),
+    playerOneRandomBtn: document.querySelector("#player1-randomize-btn"),
+    playerTwoboard: document.querySelector("#player2-board"),
+    playerTwoRandomBtn: document.querySelector("#player2-randomize-btn"),
+  };
+}
